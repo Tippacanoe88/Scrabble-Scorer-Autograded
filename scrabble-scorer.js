f3e967ead3900e2000882ed69d2b80e739f16e30
Tippacanoe88 <137940796+Tippacanoe88@users.noreply.github.com>
2023-10-09 21:16:06 -0500

    Update scrabble-scorer.js

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -85,7 +85,7 @@ const scoringAlgorithms = [
    {
       name: "Scrabble Scorer",
       description: "Original Rules",
-      scorerFunction: oldScrabbleScorer
+      scorerFunction: scrabbleScorer
    },
 ];
 
